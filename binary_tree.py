24ad62d927891e31ff44e705bd7fe907efe8a5c0
Yixuan Zhou <thomaszhou2333@gmail.com>
2020-05-22 21:19:51 -0700

    Changes

--- binary_tree.py
+++ binary_tree.py
@@ -36,6 +36,80 @@ class Node():
                 return True
             return self.right.insert(data)
 
+    """
+    def rama(self, to_replace) -> Node:
+        if self.right:
+            self.right.rama(to_replace)
+        else:
+            self.parent.right = self.left
+            return self
+
+    def remove(self, data) -> bool:
+        if data == self.data:
+            if self.left and self.right:
+                if self.parent.left == self.left:
+                    self.parent.left = self.left.rarm(self)
+                    self = self.parent.left
+                else:
+                    self.parent.right = self.right.rarm(self)
+                    self = self.parent.right
+            elif self.left:
+                if self == self.parent.left:
+                    self.parent.left = self.left
+                else:
+                    self.parent.right = self.left
+            else:
+                if self == self.parent.left:
+                    self.parent.left = self.right
+                else:
+                    self.parent.right = self.right
+        elif data < self.data:
+            if not self.left:
+                return False
+            else:
+                return self.left.remove(data)
+        else:
+            if not self.remove:
+                return False
+            else:
+                return self.remove.remove(data)
+    """
+    """
+    def rarm(self, pip):
+        if self.left:
+            return self.left.rarm(self.left)
+        else:
+            pip = self.left
+            return self
+
+    def remove(self, data, pip):
+        if data == self.data:
+            if self.left and self.right:
+                node = self.right.rarm(self.right)
+                pip = node
+                node.left = self.left
+                node.right = self.right
+            elif self.left:
+                pip = self.left
+                return data
+            elif self.right:
+                pip = self.right
+                return data
+            else:
+                pip = None
+                return data
+
+        elif data < self.data:
+            if self.left:
+                return self.left.remove(data, self.left)
+            else:
+                return None
+        else:
+            if self.right:
+                return self.right.remove(data, self.right)
+            else:
+                return None
+    """
     def write(self):
         print(self.data)
         if self.left:
